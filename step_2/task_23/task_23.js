1d17aca0b5a3963273f414fc230d405c44284193
JT <jerome891585360@gmail.com>
2016-05-05 10:14:12 +0800

    add task_23,task_24

---
+++ step_2/task_23/task_23.js
@@ -0,0 +1,102 @@
+//定义变量
+var tree_root = document.getElementById("tree-1"),
+	dfs = document.getElementById("dfs"),
+	bfs = document.getElementById("bfs"),
+	input = document.getElementById("search-content"),
+	input_search = document.getElementById("input_search"),
+	timer = null, //定时器
+	queue = [], //存储数组
+	tree_divs = document.querySelectorAll("#tree div"),
+	options = document.querySelectorAll("select option");
+
+//事件
+dfs.addEventListener("click",function(){
+	reset(); //初始化
+	dfsTree(tree_root); //深度优先搜索树
+	render(); //渲染显示
+});
+bfs.addEventListener("click",function(){
+	reset();
+	bfsTree(tree_root);
+	render();
+})
+input_search.addEventListener("click",function(){
+	reset();
+	if(options[0].selected){
+		dfsTree(tree_root)
+	} else{
+		bfsTree(tree_root);
+	}
+	render(input.value.trim());
+})
+//函数
+function reset(){
+	clearInterval(timer);
+	queue = [];
+	// tree_divs.forEach(function(item){
+	// 	item.style.background = "#fff";
+	// });
+	for(var i=0; i<tree_divs.length; i++){
+		tree_divs[i].style.background = "#fff";
+	}
+}
+function dfsTree(node){
+	if(node.nodeName == "DIV"){
+		queue.push(node);
+		dfsTree(node.firstChild);
+	}
+	if(!(node.nextSibling == null)){
+		dfsTree(node.nextSibling);	
+	}
+}
+function bfsTree(node){
+	if(node.nodeName == "DIV"){
+		queue.push(node);
+	}
+	bfsTreeRecur(node);
+}
+function bfsTreeRecur(node){
+	var nodeChilds = node.childNodes;
+	for(var i=0; i<nodeChilds.length; i++){
+		if(nodeChilds[i].nodeName == "DIV"){
+			queue.push(nodeChilds[i]);
+		}
+	}
+	for(var i=0; i<nodeChilds.length; i++){
+		if(nodeChilds[i].nodeName == "DIV"){
+			bfsTreeRecur(nodeChilds[i]);
+		}
+	}
+}
+
+function render(str){
+	var selectIndex;
+	if (str){
+		for(var i=0; i<queue.length; i++){
+			// console.log(item);
+			// console.log(item.innerText.split("\n")[0]);
+			if(queue[i].innerText.split("\n")[0] == str){
+				selectIndex = i;
+				break;
+			}
+		};
+		if( selectIndex == null){
+			alert ("输入信息有误！");
+			return;
+		} 	
+	} 
+	var i = 0;
+	timer = setInterval(function(){
+		if(i >= 1 ){
+			queue[i-1].style.background = "#fff";			
+		}
+		if( i == queue.length){
+			clearInterval(timer);
+		} else {
+			queue[i].style.background = "#60BC94";
+			if( i++ == selectIndex){
+				clearInterval(timer);
+			}
+		}
+	},300);
+}
---
+++ step_2/task_24/task_24.js
@@ -0,0 +1,136 @@
+//定义变量
+var tree_root = document.getElementById("tree-1"),
+	dfs = document.getElementById("dfs"),
+	bfs = document.getElementById("bfs"),
+	input = document.getElementById("search-content"),
+	input_search = document.getElementById("input_search"),
+	timer = null, //定时器
+	queue = [], //存储数组
+	tree_divs = document.querySelectorAll("#tree div"),//树的所有div元素
+	options = document.querySelectorAll("select option"),//选项
+	del_btn = document.getElementById("del-btn"),
+	add_btn = document.getElementById("add-btn"),
+	add_input = document.getElementById("add-content"),
+	selectNode;
+
+//事件
+//深度优先搜索遍历
+dfs.addEventListener("click",function(){
+	reset(); //初始化
+	dfsTree(tree_root); //深度优先搜索树
+	render(); //渲染显示
+});
+//广度优先搜索遍历
+bfs.addEventListener("click",function(){
+	reset();
+	bfsTree(tree_root);
+	render();
+})
+//输入搜索
+input_search.addEventListener("click",function(){
+	reset();
+	if(options[0].selected){
+		dfsTree(tree_root)
+	} else{
+		bfsTree(tree_root);
+	}
+	render(input.value.trim());
+})
+//选中
+
+tree_root.addEventListener("click",function(e){
+	if(selectNode){
+		selectNode.className -= " select-node";
+	}
+	selectNode = e.target;
+	selectNode.className += " select-node";
+})
+//选中删除
+del_btn.addEventListener("click",function(){
+	if(selectNode){
+		selectNode.parentNode.removeChild(selectNode);
+	}
+})
+//选中添加子节点
+add_btn.addEventListener("click",function(){
+	var input_value = add_input.value.trim();
+	if(selectNode){
+		if(input_value){
+			selectNode.innerHTML += "<div id='"+input_value+"'>"+input_value+"</div>";
+		}
+	}
+})
+
+
+//函数
+//初始化
+function reset(){
+	clearInterval(timer);
+	queue = [];
+	selectNode = null;
+	for(var i=0; i<tree_divs.length; i++){
+		tree_divs[i].style.background = "#fff";
+	}
+}
+//dfs
+function dfsTree(node){
+	if(node.nodeName == "DIV"){
+		queue.push(node);
+		dfsTree(node.firstChild);
+	}
+	if(!(node.nextSibling == null)){
+		dfsTree(node.nextSibling);	
+	}
+}
+//bfs
+function bfsTree(node){
+	if(node.nodeName == "DIV"){
+		queue.push(node);
+	}
+	bfsTreeRecur(node);
+}
+function bfsTreeRecur(node){
+	var nodeChilds = node.childNodes;
+	for(var i=0; i<nodeChilds.length; i++){
+		if(nodeChilds[i].nodeName == "DIV"){
+			queue.push(nodeChilds[i]);
+		}
+	}
+	for(var i=0; i<nodeChilds.length; i++){
+		if(nodeChilds[i].nodeName == "DIV"){
+			bfsTreeRecur(nodeChilds[i]);
+		}
+	}
+}
+//渲染显示
+function render(str){
+	var selectIndex;
+	if (str){
+		for(var i=0; i<queue.length; i++){
+			// console.log(item);
+			// console.log(item.innerText.split("\n")[0]);
+			if(queue[i].innerText.split("\n")[0] == str){
+				selectIndex = i;
+				break;
+			}
+		};
+		if( selectIndex == null){
+			alert ("输入信息有误！");
+			return;
+		} 	
+	} 
+	var i = 0;
+	timer = setInterval(function(){
+		if(i >= 1 ){
+			queue[i-1].style.background = "#fff";			
+		}
+		if( i == queue.length){
+			clearInterval(timer);
+		} else {
+			queue[i].style.background = "#60BC94";
+			if( i++ == selectIndex){
+				clearInterval(timer);
+			}
+		}
+	},300);
+}
